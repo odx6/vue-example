b56c76d69256cd1924db60ae427e284cbf274d4e
Evrardo Alvaro Agustin Cruz <Ever234.ea@gmail.com>
2024-10-22 00:15:18 -0600

    curso de vue

---
+++ vue curso/vue-2/app.js
@@ -0,0 +1,7 @@
+const app= new  Vue({
+    el:'#main',
+    data:{
+        texto:''
+    }
+
+})
---
+++ vue curso/vue-2/directivas/app.js
@@ -0,0 +1,16 @@
+const app= new  Vue({
+    el:'#main',
+    data:{
+        conectado:true,
+       si:false,
+       edad:17,
+       lista:['azul', 'rojo','negro','morado'],
+       listaDePersonas:[
+        {nombre: 'Everardo' , Edad:24, sexo:'Masculino'},
+        {nombre: 'jesus' , Edad:24, sexo:'Masculino'},
+        {nombre: 'Cesar' , Edad:24, sexo:'Masculino'},
+        {nombre: 'Alan ', Edad:24, sexo:'Masculino'},
+       ]
+    }
+
+})
